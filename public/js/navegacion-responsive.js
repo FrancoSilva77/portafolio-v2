930bd471949d94fb6da54f4eef8172189a51772b
Franco Silva <franco_tigres18@hotmail.com>
2023-01-20 12:28:29 -0600

    Funcionalidad scroll-bar add

--- public/js/navegacion-responsive.js
+++ public/js/navegacion-responsive.js
@@ -1,6 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
   eventListeners();
+  scrollNav();
 });
+
+function scrollNav() {
+  const enlaces = document.querySelectorAll(".navegacion a");
+  enlaces.forEach((enlace) => {
+    enlace.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const seccionScroll = e.target.attributes.href.value;
+      const seccion = document.querySelector(seccionScroll);
+      seccion.scrollIntoView({ behavior: "smooth" });
+    });
+  });
+}
 
 
 /* Menu hamburgesa */
